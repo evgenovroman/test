b203790bc70910ea436f1e7bf47dfe2bc7087e87
Roman <evgenov.roman@mail.ru>
2019-10-05 14:11:26 +0300

    Это первый коммит

---
+++ JavaScript уроки/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+console.log(4/0);
+console.log('string'*9);
+
+let persone = {
+    name: "Jonh",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum', 'png' , 'apple.bmp'];
+
+console.log(arr[1]);
+
+// alert("Hello,world!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = +prompt("Есть ли вам 18?", "Да");
+console.log(typeof (answer));
+
+console.log("array" + "-it's object");
