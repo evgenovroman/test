b17a72657d124fb23c7a27125139c0f9773e4134
Roman <evgenov.roman@mail.ru>
2019-10-06 11:49:27 +0300

    Created new lesson1

---
+++ JavaScript уроки/Lesson1/script.js
@@ -0,0 +1,51 @@
+/*let woo = 12;
+
+console.log(4/0);
+console.log('string'*9);
+
+let persone = {
+    name: "Jonh",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum', 'png' , 'apple.bmp'];
+
+console.log(arr[1]);
+
+// alert("Hello,world!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = +prompt("Есть ли вам 18?", "Да");
+console.log(typeof (answer));
+
+console.log("array" + "-it's object");*/
+
+let num = 50;
+
+// if (num < 49) {
+//     console.log("Неверно")
+// } else if (num > 100) {
+//     console.log("Много")
+// } else {
+//     console.log("Действительно")
+// };
+
+// (num == 50) ? console.log("Верно!") : console.log("Неверно!");
+
+switch(num) {
+    case num < 49:
+        console.log("Неверно");
+        break;
+    case 50:
+        console.log("Верно");
+        break;
+    case num > 50:
+        console.log("Многовато");
+        break;
+    
+}
