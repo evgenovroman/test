6a83ca47400a34db6c5b07431e9a359e2b45b1ee
Roman <evgenov.roman@mail.ru>
2019-10-06 18:28:24 +0300

    Update homework

--- JavaScript уроки/Homework_lesson1/script.js
+++ JavaScript уроки/Homework_lesson1/script.js
@@ -1,17 +1,42 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?" , ""),
-    time  = +prompt("Введите дату в формате YYYY-MM-DD", "2019-12-30");
-let appdata = {
+
+var nowDate = new Date();
+let money = prompt("Ваш бюджет на месяц?" , "");
+    while (money === null || money.length == 0 || money.length > 10) {
+        alert("Вы не ввели свой бюджет на месяц или ввели слишком большое число");
+        money = prompt("Ваш бюджет на месяц?" , "");
+    };
+    parseInt(money);
+let time  = prompt("Введите дату в формате YYYY-MM-DD", nowDate),
+    appData = {
     budget   : money,
     timeData : time,
-    expenses : {
-        firstAnswer  : +prompt("Введите обязательную статью расходов в этом месяце"),
-        secondAnswer : +prompt("Во сколько это обойдется?")
-    },
+    expenses : {},
     optionalExpenses : {},
     income : [],
     savings : false
 };
 
+
+for (let i = 0; i < 2; i++) {
+    let firstAnswer  = prompt("Введите обязательную статью расходов в этом месяце" , ""),
+        secondAnswer = prompt("Во сколько обойдется?" , "");
+
+
+        if (firstAnswer != null && secondAnswer != null && firstAnswer.length > 0 && secondAnswer.length > 0){
+            alert("done");
+        }
+        else {
+            while (firstAnswer === null || secondAnswer === null || firstAnswer.length == 0 || secondAnswer.length == 0 || firstAnswer.length > 10) {
+                alert("Вы ничего не ввели или ввели слишком большое число"); 
+                firstAnswer  = prompt("Введите обязательную статью расходов в этом месяце" , ""),
+                secondAnswer = prompt("Во сколько обойдется?" , "");   
+            };
+        };
+    appData.expenses[firstAnswer] = secondAnswer;
+};
+
 alert("Ваш бюджет на один день: " + Math.floor(money / 30));
+
+console.log(appData);
--- JavaScript уроки/Lesson1/script.js
+++ JavaScript уроки/Lesson1/script.js
@@ -25,7 +25,7 @@ console.log(typeof (answer));
 
 console.log("array" + "-it's object");*/
 
-let num = 50;
+// let num = 50;
 
 // if (num < 49) {
 //     console.log("Неверно")
@@ -37,15 +37,33 @@ let num = 50;
 
 // (num == 50) ? console.log("Верно!") : console.log("Неверно!");
 
-switch(num) {
-    case num < 49:
-        console.log("Неверно");
-        break;
-    case 50:
-        console.log("Верно");
-        break;
-    case num > 50:
-        console.log("Многовато");
-        break;
+// switch(num) {
+//     case num < 49:
+//         console.log("Неверно");
+//         break;
+//     case 50:
+//         console.log("Верно");
+//         break;
+//     case num > 50:
+//         console.log("Многовато");
+//         break;
     
-}
+// }
+
+// let num = 50;
+
+// while (num < 60) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     ++num;
+//     console.log(num);
+// } 
+// while (num < 60);
+
+// for (let i = 0; num < 60; i++) {
+//     console.log(num , i);
+let money = prompt("asfdsg", "");
+console.log(typeof(money));
